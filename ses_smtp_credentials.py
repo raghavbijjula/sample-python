c5e8a50daf819b3f18982c92eec36d2b569e3d82
Raghav Bijjula <rbijjula@thomsonreuters.com>
2020-09-10 19:48:06 +0530

    updated

--- ses_smtp_credentials.py
+++ ses_smtp_credentials.py
@@ -12,7 +12,7 @@ import struct
 
 def main():
     #access_key_id, secret_access_key = get_access_creds()
-    username, password = get_smtp_creds('97xXS6aLf9WfRvHr', 'CM9CC6bmxZqQjcCgMj2qT75At11111312')
+    username, password = get_smtp_creds('97xXS6aLf9WfRvHrbnmb', 'CM9CC6bmxZqQjcCgMj2qT75At11111312')
 
     print('SMTP Username: %s' % username)
     print('SMTP Password: %s' % password)
