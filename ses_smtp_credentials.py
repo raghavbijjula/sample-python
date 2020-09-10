e0e24e0f9d35922700f3ef65c82727159c64403e
Raghav Bijjula <rbijjula@thomsonreuters.com>
2020-09-10 18:28:20 +0530

    added script

---
+++ ses_smtp_credentials.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import base64
+import hashlib
+import hmac
+import json
+import struct
+#import urllib2
+
+
+def main():
+    #access_key_id, secret_access_key = get_access_creds()
+    username, password = get_smtp_creds('XXXXXXXXX', 'XXXXXXXXXXXXXXXXXXX')
+
+    print('SMTP Username: %s' % username)
+    print('SMTP Password: %s' % password)
+
+
+
+def get_smtp_creds(access_key_id, secret_access_key):
+    message = 'SendRawEmail'
+    version = 0x02
+
+    sig= hmac.new(
+        secret_access_key,
+        msg=message,
+        digestmod=hashlib.sha256)
+    sig_bytes = sig.digest()
+    sig_and_version_bytes = (struct.pack('B', version) + sig_bytes)
+    smtp_password = base64.b64encode(sig_and_version_bytes)
+
+    return access_key_id, smtp_password
+
+if __name__ == '__main__':
+    main()
